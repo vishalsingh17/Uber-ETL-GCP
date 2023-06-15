8e1cc94c6d9419e99b71eab9269b93346123a28b
vishalsingh17 <vishal170997@gmail.com>
2023-06-15 15:46:58 +0530

    Updated load.py

--- mage-files/load.py
+++ mage-files/load.py
@@ -16,7 +16,6 @@ def export_data_to_big_query(data, **kwargs) -> None:
 
     Docs: https://docs.mage.ai/design/data-loading#bigquery
 
-    
     """
     config_path = path.join(get_repo_path(), 'io_config.yaml')
     config_profile = 'default'
